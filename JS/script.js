695cae190daf15e69e46c5595a445a49d7f059bd
Shanax21 <shahnawaz8321026@gmail.com>
2022-08-28 21:49:30 +0600

    Added functions

---
+++ JS/script.js
@@ -0,0 +1,145 @@
+var counter=1;
+document.getElementById('button').addEventListener('click',function(){
+    if (counter>5){
+        alert("Maximum Players have been selected.");
+    }
+    else{
+        const list = document.getElementById('lister');
+        const li = document.createElement('li');
+        const name = document.getElementById('name');
+        const namevalue = name.innerText;
+        li.innerText=namevalue;
+        li.style.fontSize="15px";
+        list.append(li);
+        counter++;
+    }
+    
+})
+document.getElementById('button2').addEventListener('click',function(){
+    if (counter>5){
+        alert("Maximum Players have been selected.");
+    }
+    else{
+        const list = document.getElementById('lister');
+        const li = document.createElement('li');
+        const name = document.getElementById('name2');
+        const namevalue = name.innerText;
+        li.innerText=namevalue;
+        li.style.fontSize="15px";
+        list.append(li);
+        counter++;
+    }
+    
+})
+document.getElementById('button3').addEventListener('click',function(){
+    if (counter>5){
+        alert("Maximum Players have been selected.");
+    }
+    else{
+        const list = document.getElementById('lister');
+        const li = document.createElement('li');
+        const name = document.getElementById('name3');
+        const namevalue = name.innerText;
+        li.innerText=namevalue;
+        li.style.fontSize="15px";
+        list.append(li);
+        counter++;
+    }
+    
+})
+document.getElementById('button4').addEventListener('click',function(){
+    if (counter>5){
+        alert("Maximum Players have been selected.");
+    }
+    else{
+        const list = document.getElementById('lister');
+        const li = document.createElement('li');
+        const name = document.getElementById('name4');
+        const namevalue = name.innerText;
+        li.innerText=namevalue;
+        li.style.fontSize="15px";
+        list.append(li);
+        counter++;
+    }
+    
+})
+document.getElementById('button5').addEventListener('click',function(){
+    if (counter>5){
+        alert("Maximum Players have been selected.");
+    }
+    else{
+        const list = document.getElementById('lister');
+        const li = document.createElement('li');
+        const name = document.getElementById('name5');
+        const namevalue = name.innerText;
+        li.innerText=namevalue;
+        li.style.fontSize="15px";
+        list.append(li);
+        counter++;
+    }
+    
+})
+document.getElementById('button6').addEventListener('click',function(){
+    if (counter>5){
+        alert("Maximum Players have been selected.");
+    }
+    else{
+        const list = document.getElementById('lister');
+        const li = document.createElement('li');
+        const name = document.getElementById('name6');
+        const namevalue = name.innerText;
+        li.innerText=namevalue;
+        li.style.fontSize="15px";
+        list.append(li);
+        counter++;
+    }
+    
+})
+document.getElementById('button7').addEventListener('click',function(){
+    if (counter>5){
+        alert("Maximum Players have been selected.");
+    }
+    else{
+        const list = document.getElementById('lister');
+        const li = document.createElement('li');
+        const name = document.getElementById('name7');
+        const namevalue = name.innerText;
+        li.innerText=namevalue;
+        li.style.fontSize="15px";
+        list.append(li);
+        counter++;
+    }
+    
+})
+document.getElementById('button8').addEventListener('click',function(){
+    if (counter>5){
+        alert("Maximum Players have been selected.");
+    }
+    else{
+        const list = document.getElementById('lister');
+        const li = document.createElement('li');
+        const name = document.getElementById('name8');
+        const namevalue = name.innerText;
+        li.innerText=namevalue;
+        li.style.fontSize="15px";
+        list.append(li);
+        counter++;
+    }
+    
+})
+document.getElementById('button9').addEventListener('click',function(){
+    if (counter>5){
+        alert("Maximum Players have been selected.");
+    }
+    else{
+        const list = document.getElementById('lister');
+        const li = document.createElement('li');
+        const name = document.getElementById('name9');
+        const namevalue = name.innerText;
+        li.innerText=namevalue;
+        li.style.fontSize="15px";
+        list.append(li);
+        counter++;
+    }
+    
+})
